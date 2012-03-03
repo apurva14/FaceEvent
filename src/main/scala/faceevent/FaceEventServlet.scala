86ce032060c8b4c93daf1ab3d5a390d2fe0bfd6d
apurva14 <jaiswal.apurva@gmail.com>
2012-03-03 03:56:24 +0200

    heroku second

---
+++ src/main/scala/faceevent/FaceEventServlet.scala
@@ -0,0 +1,21 @@
+package faceevent
+
+import org.scalatra._
+import net.liftweb.json._
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: apurva
+ * Date: 3/3/12
+ * Time: 1:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class FaceEventServlet extends ScalatraServlet {
+  implicit val formats = DefaultFormats
+  post("/event") {
+
+  }
+
+  def render(content: AnyRef) = net.liftweb.json.Serialization.write(content)
+}
